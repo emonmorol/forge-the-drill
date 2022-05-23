da03690f743a97a9953fa36f5cda1b201aeb2823
emonmorol <ekramulhasane69@gmail.com>
2022-05-24 00:18:31 +0600

    make admin page featured

--- src/Pages/DashboardPage/MakeAdmin/UserRow.js
+++ src/Pages/DashboardPage/MakeAdmin/UserRow.js
@@ -1,15 +1,34 @@
 import React from "react";
+import { toast } from "react-toastify";
+import primaryAxios from "../../../Api/primaryAxios";
 
 const UserRow = ({ user, refetch, index }) => {
-  const { name, email, role } = user;
+  const { name, email, role, _id } = user;
+
+  const handleMakeAdmin = (id) => {
+    (async () => {
+      const { data } = await primaryAxios.put(`/user-role?id=${id}`, {
+        role: "admin",
+      });
+      if (data.success) {
+        toast.success(`${name} Is Now Admin`);
+        refetch();
+      }
+    })();
+  };
+
   return (
     <tr>
       <td>{index + 1}</td>
       <td>{name}</td>
       <td>{email}</td>
-      <td>{role ? role : "User"}</td>
+      <td>{role ? "Admin" : "User"}</td>
       <td>
-        <button className="btn btn-xs btn-outline btn-primary hover:text-white rounded-full">
+        <button
+          onClick={() => handleMakeAdmin(_id)}
+          className="btn btn-xs btn-outline btn-primary hover:text-white rounded-full"
+          disabled={role}
+        >
           Make Admin
         </button>
       </td>
